5156597ecc1cb137a56cc333dc3b335909561194
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-11 00:50:15 +0300

    EPS fix.

--- lab_01_06_05/main.c
+++ lab_01_06_05/main.c
@@ -48,7 +48,7 @@ float det(float x1, float y1, float x2, float y2, float x3, float y3, float x4, 
 int point_check(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
 {
     return fabs((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) <= EPS &&
-        fabs((x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1)) <= EPS;
+           fabs((x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1)) <= EPS;
 }
 
 int cramers_rule(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float det)
@@ -58,9 +58,11 @@ int cramers_rule(float x1, float y1, float x2, float y2, float x3, float y3, flo
     b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2);
     u_a = a / det;
     u_b = b / det;
-    if (u_a >= EPS && fabs(u_a - 1) <= EPS && u_b >= EPS && (u_b - 1) <= EPS)
+    if (u_a >= EPS && u_a <= EPS + 1 && u_b >= EPS && u_b <= EPS + 1)
         return 1;
     else
         return 0;
 }
+
+
 
