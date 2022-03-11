b1346e4d713392415b5adc63a93e493bce1deb45
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-11 17:55:30 +0300

    Sixth refactoring.

--- lab_01_06_05/main.c
+++ lab_01_06_05/main.c
@@ -1,65 +1,57 @@
 #include <stdio.h>
-#include <math.h>
-#define EPS 1e-7
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-float det(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
-int point_check(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
-int cramers_rule(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float det);
+int area(int ax, int ay, int bx, int by, int cx, int cy);
+void swap(int *a, int *b);
+int intersect_sub(int a, int b, int c, int d);
+int intersect_main(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy);
 
 int main(void)
 {
-    float xp, yp, xq, yq;
-    float xr, yr, xs, ys;
+    int px, py, qx, qy;
+    int rx, ry, sx, sy;
     int rc1, rc2;
 
-    rc1 = scanf("%f%f%f%f", &xp, &yp, &xq, &yq);
+    rc1 = scanf("%d%d%d%d", &px, &py, &qx, &qy);
     if (rc1 != 4)
         return -100;
 
-    rc2 = scanf("%f%f%f%f", &xr, &yr, &xs, &ys);
+    rc2 = scanf("%d%d%d%d", &rx, &ry, &sx, &sy);
     if (rc2 != 4)
         return -100;
 
-    float determinant = det(xp, yp, xq, yq, xr, yr, xs, ys);
-    if (fabs(determinant) <= EPS)
-    {
-        if (point_check(xp, yp, xq, yq, xr, yr, xs, ys))
-            printf("1");
-        else
-            printf("0");
-    }
-    else
-    {
-        if (cramers_rule(xp, yp, xq, yq, xr, yr, xs, ys, determinant))
-            printf("1");
-        else
-            printf("0");
-    }
+    printf("%d", intersect_main(px, py, qx, qy, rx, ry, sx, sy));
     return 0;
 }
 
-float det(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+int area(int ax, int ay, int bx, int by, int cx, int cy)
 {
-    return (y4 - y3) * (x1 - x2) - (x4 - x3) * (y1 - y2);
+    return (bx - ax) * (cy - ay) - (by - ay) * (cx - ax);
 }
 
-int point_check(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+void swap(int *a, int *b)
 {
-    return fabs((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) <= EPS &&
-           fabs((x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1)) <= EPS;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-int cramers_rule(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float det)
+int intersect_sub(int a, int b, int c, int d)
 {
-    float u_a, u_b, a, b;
-    a = (x4 - x2) * (y4 - y3) - (x4 - x3) * (y4 - y2);
-    b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2);
-    u_a = a / det;
-    u_b = b / det;
-    if (u_a + EPS >= 0 && u_a - EPS <= 1 && u_b + EPS >= 0 && u_b - EPS <= 1)
-        return 1;
-    else
-        return 0;
+    if (a > b)
+        swap(&a, &b);
+    if (c > d)
+        swap(&c, &d);
+    return MAX(a, c) <= MIN(b, d);
+}
+
+int intersect_main(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy)
+{
+    return intersect_sub(ax, bx, cx, dx)
+        && intersect_sub(ay, by, cy, dy)
+        && area(ax, ay, bx, by, cx, cy) * area(ax, ay, bx, by, dx, dy) <= 0
+        && area(cx, cy, dx, dy, ax, ay) * area(cx, cy, dx, dy, bx, by) <= 0;
 }
 
 
