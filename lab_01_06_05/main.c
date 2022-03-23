09e961e8184807a3d0456b58d11cccb4b638a577
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-24 01:28:44 +0300

    Int check

--- lab_01_06_05/main.c
+++ lab_01_06_05/main.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
 #include <math.h>
-#define EPS 0.00001
+#define EPS 1E-5
 
-int intersect_main(double ax, double ay, double bx, double by, double cx, double cy, double dx, double dy);
-double v_create(double ax, double ay, double bx, double by);
+int intersect_main(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy);
+int v_create(int ax, int ay, int bx, int by);
 
 int main(void)
 {
-    double px, py, qx, qy;
-    double rx, ry, sx, sy;
+    int px, py, qx, qy;
+    int rx, ry, sx, sy;
     int check_first_inp, check_second_inp;
 
     printf("Введите координаты первого отрезка: ");
-    check_first_inp = scanf("%lf%lf%lf%lf", &px, &py, &qx, &qy);
+    check_first_inp = scanf("%d%d%d%d", &px, &py, &qx, &qy);
     if (check_first_inp != 4)
         return -100;
 
     printf("Введите координаты второго отрезка: ");
-    check_second_inp = scanf("%lf%lf%lf%lf", &rx, &ry, &sx, &sy);
+    check_second_inp = scanf("%d%d%d%d", &rx, &ry, &sx, &sy);
     if (check_second_inp != 4)
         return -100;
 
-    if ((fabs(px - qx) < EPS && fabs(py - qy) < EPS) || (fabs(rx - sx) < EPS && fabs(ry - sy) < EPS))
+    if ((px == qx && py == qy) || (rx == sx && ry == sy))
         return -100;
 
     printf("%d", intersect_main(px, py, qx, qy, rx, ry, sx, sy));
     return 0;
 }
 
-double v_create(double ax, double ay, double bx, double by)
+int v_create(int ax, int ay, int bx, int by)
 {
     return ax * ay - bx * by;
 }
 
-int intersect_main(double ax, double ay, double bx, double by, double cx, double cy, double dx, double dy)
+int intersect_main(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy)
 {
-    double first_vector = v_create(dx - cx, dy - cy, ax - cx, ay - cy);
-    double second_vector = v_create(dx - cx, dy - cy, bx - cx, by - cy);
-    double third_vector = v_create(bx - ax, by - ay, cx - ax, cy - ay);
-    double fourth_vector = v_create(bx - ax, by - ay, dx - ax, dy - ay);
+    int first_vector = v_create(dx - cx, dy - cy, ax - cx, ay - cy);
+    int second_vector = v_create(dx - cx, dy - cy, bx - cx, by - cy);
+    int third_vector = v_create(bx - ax, by - ay, cx - ax, cy - ay);
+    int fourth_vector = v_create(bx - ax, by - ay, dx - ax, dy - ay);
     if ((first_vector * second_vector < 0) && (third_vector * fourth_vector < 0))
         return 1;
     else
