9e6a1e1dd44ce1b82e9e909c3b5d5215ed1ae66c
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-23 17:13:47 +0300

    Hotfix

--- lab_01_07_05/main.c
+++ lab_01_07_05/main.c
@@ -1,63 +1,58 @@
 #include <stdio.h>
 #include <math.h>
 
-float calculate_relative(float f_x, float s_x);
-float calculate_absolute(float f_x, float s_x);
-float calculate_f_x(float x);
-float calculate_s_x(float x, float eps);
+double calculate_relative(double f_x, double s_x);
+double calculate_absolute(double f_x, double s_x);
+double calculate_f_x(double x);
+double calculate_s_x(double x, double eps);
 
 int main(void)
 {
-    float x, eps;
-    printf("Введите x, eps");
-    int check_first_inp;
-    check_first_inp = scanf("%f%f", &x, &eps);
-    if (check_first_inp != 2 || fabsf(x) >= 1)
-    {
-        return -100;
-    }
-    float s_x = calculate_s_x(x, eps), f_x = calculate_f_x(x);
-    printf("%f%f%f%f", s_x, f_x, calculate_absolute(f_x, s_x), calculate_relative(f_x, s_x));
+    double x, eps;
+    printf("Введите x, eps: \n");
+    scanf("%lf%lf", &x, &eps);
+    double s_x = calculate_s_x(x, eps), f_x = calculate_f_x(x);
+    printf("%f %f %f %f", s_x, f_x, calculate_absolute(f_x, s_x), calculate_relative(f_x, s_x));
     return 0;
 }
 
-float calculate_relative(float f_x, float s_x)
+double calculate_relative(double f_x, double s_x)
 {
-    return fabsf(f_x - s_x) / fabsf(f_x);
+    return fabs(f_x - s_x) / fabs(f_x);
 }
 
-float calculate_absolute(float f_x, float s_x)
+double calculate_absolute(double f_x, double s_x)
 {
-    return fabsf(f_x - s_x);
+    return fabs(f_x - s_x);
 }
 
-float calculate_f_x(float x)
+double calculate_f_x(double x)
 {
-    return 1 / sqrtf(1 - pow(x, 2));
+    return 1 / sqrt(1 - pow(x, 2));
 }
 
-float calculate_s_x(float x, float eps)
+double calculate_s_x(double x, double eps)
 {
-    float sum = 1;
-
-    int numerator = 1;
-    int denominator = 2;
+    double sum = 1;
+    double numerator = 1;
+    double denominator = 2;
     int power = 2;
-
     int counter_numerator = 1;
     int counter_denominator = 2;
 
-    float new = numerator * pow(x, power) / denominator;
+    double new = (counter_numerator * pow(x, power)) / denominator;
+    sum += new;
 
-    while (fabsf(eps - new) < 0)
+    while (fabs(new) > eps)
     {
         power += 2;
-        counter_numerator += 2;
 
+        counter_numerator += 2;
         counter_denominator += 2;
+
         numerator *= counter_numerator;
         denominator *= counter_denominator;
-        new = numerator * pow(x, power) / denominator;
+        new = (numerator * pow(x, power)) / denominator;
 
         sum += new;
     }
