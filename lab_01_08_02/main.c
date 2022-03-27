6d30e96294f950be18422fa615ce6ffd1b70ef18
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-27 21:45:27 +0300

    Eight task fixed

--- lab_01_08_02/main.c
+++ lab_01_08_02/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 void print_binary(unsigned int n);
+u_int32_t bit_rol(u_int32_t x, int n);
 
 int main(void)
 {
-    unsigned int x;
+    u_int32_t x;
     int n, check_input;
-    check_input = scanf("%d%d", &x, &n);
+    check_input = scanf("%u %d", &x, &n);
     if (check_input != 2 || n < 0)
     {
         printf("Error: Wrong Input");
         return -100;
     }
-
-    for (int i = 0; i < n; ++i)
-    {
-        x <<= 1;
-    }
+    x = bit_rol(x, n);
     printf("Result: ");
     print_binary(x);
     return 0;
@@ -29,3 +26,7 @@ void print_binary(unsigned int n)
     printf("%d", n % 2);
 }
 
+u_int32_t bit_rol(u_int32_t x, int n)
+{
+    return x << n | x >> (32 - n);
+}
