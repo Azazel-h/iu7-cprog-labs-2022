ae97be1a6242e5e2964ef7ad453ab82421667467
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-27 15:51:43 +0300

    Memory  fix

--- lab_01_09_02/main.c
+++ lab_01_09_02/main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 double calculate_new(double x, int n);
+double calculate_g_x(double sum, int n);
 
 int main(void)
 {
@@ -9,8 +10,8 @@ int main(void)
     int num_of_x = 0, check_input;
 
     check_input = scanf("%lf", &x);
-    if (check_input != 1)
-        return -1;
+    if (check_input != 1 || x < 0)
+        return -100;
 
     while (x >= 0)
     {
@@ -19,11 +20,16 @@ int main(void)
         scanf("%lf", &x);
     }
 
-    printf("%lf", (1 / num_of_x) * sum);
+    printf("%lf", calculate_g_x(sum, num_of_x));
     return 0;
 }
 
 double calculate_new(double x, int n)
 {
     return sqrt(n + x);
+}
+
+double calculate_g_x(double sum, int n)
+{
+    return (1 / n) * sum;
 }
