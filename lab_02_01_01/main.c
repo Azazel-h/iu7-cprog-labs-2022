e9d59af4c19950eef2a7d3523c8043a393c8fd78
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-04 00:28:38 +0300

    First task testing

--- lab_02_01_01/main.c
+++ lab_02_01_01/main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #define STANDARD_SIZE 10
+#define OK 0
+#define INPUT_ERROR -100
+
+int find_product(int* arr);
 
 int main(void)
 {
+    int status_code = OK;
     int arr[STANDARD_SIZE];
     for (int i = 0; i < STANDARD_SIZE; ++i)
     {
-        scanf("%d", &arr[i])
+        int check_input = scanf("%d", arr + i);
+        if (check_input != 1)
+            status_code = INPUT_ERROR;
     }
+    printf("%d", find_product(arr));
+    return status_code;
+}
+
+int find_product(int* arr)
+{
+    int product = 1;
     for (int i = 0; i < STANDARD_SIZE; ++i)
     {
-        printf("%d", arr[i])
+        if (arr[i] % 2 != 0)
+            product *= arr[i];
     }
+    return product;
 }
