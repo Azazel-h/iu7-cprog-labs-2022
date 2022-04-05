fdbfe9274565c5110829f37c076484d794065f96
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-05 14:09:02 +0300

    Refactoring a part of reading

--- lab_02_01_01/main.c
+++ lab_02_01_01/main.c
@@ -3,25 +3,22 @@
 #define OK 0
 #define INPUT_ERROR -100
 
-int find_product(int *arr, int n, int *product);
+int find_product(int *arr, int n);
 int read_array(int *arr, int n);
-
 
 int main(void)
 {
     int arr[STANDARD_SIZE];
-    int status_code = OK, check_input, product = 1;
-    int n;
+    int status_code = OK, check_input, n;
 
     check_input = scanf("%d", &n);
     if (check_input != 1 || n < 1 || n > 10)
         status_code = INPUT_ERROR;
 
-    if (status_code == OK && read_array(arr, n) == OK)
-        status_code = find_product(arr, n, &product);
-
+    status_code = read_array(arr, n);
     if (status_code == OK)
-        printf("%d", product);
+        printf("%d", find_product(arr, n));
+
     return status_code;
 }
 
@@ -37,15 +34,14 @@ int read_array(int *arr, int n)
     return status_code;
 }
 
-int find_product(int *arr, int n, int *product)
+int find_product(int *arr, int n)
 {
-    int status_code = OK;
+    int product = 1;
     for (int i = 0; i < n; ++i)
     {
         if (arr[i] % 2 != 0)
-        {
-            *product *= arr[i];
-        }
+            product *= arr[i];
     }
-    return status_code;
+    return product;
 }
+
