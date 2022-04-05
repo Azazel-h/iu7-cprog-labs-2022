b81d438c86f9c09204864049ca2220cc53289e9b
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-05 11:07:46 +0300

    Fix Odd check

--- lab_02_01_01/main.c
+++ lab_02_01_01/main.c
@@ -3,35 +3,56 @@
 #define OK 0
 #define INPUT_ERROR -100
 
-int find_product(int *arr, int n);
+int find_product(int *arr, size_t n, int *product);
+int read_array(int *arr, size_t n);
+
 
 int main(void)
 {
     int arr[STANDARD_SIZE];
-    int status_code = OK, check_input, n;
+    int status_code = OK, check_input, product = 1;
+    size_t n;
 
-    check_input = scanf("%d", &n);
+    check_input = scanf("%ld", &n);
     if (check_input != 1 || n < 1 || n > 10)
         status_code = INPUT_ERROR;
 
-    for (int i = 0; i < n; ++i)
+    if (status_code == OK && read_array(arr, n) == OK)
+        status_code = find_product(arr, n, &product);
+    else
+        status_code = INPUT_ERROR;
+
+    if (status_code == OK)
+        printf("%d", product);
+    return status_code;
+}
+
+int read_array(int *arr, size_t n)
+{
+    int status_code = OK, check_input;
+    for (size_t i = 0; i < n; ++i)
     {
         check_input = scanf("%d", arr + i);
         if (check_input != 1)
             status_code = INPUT_ERROR;
     }
-    if (status_code == OK)
-        printf("%d", find_product(arr, n));
+
     return status_code;
 }
 
-int find_product(int *arr, int n)
+int find_product(int *arr, size_t n, int *product)
 {
-    int product = 1;
-    for (int i = 0; i < n; ++i)
+    int is_odd = 0, status_code = OK;
+    for (size_t i = 0; i < n; ++i)
     {
         if (arr[i] % 2 != 0)
-            product *= arr[i];
+        {
+            *product *= arr[i];
+            is_odd = 1;
+        }
     }
-    return product;
+    if (is_odd != 1)
+        status_code = INPUT_ERROR;
+
+    return status_code;
 }
