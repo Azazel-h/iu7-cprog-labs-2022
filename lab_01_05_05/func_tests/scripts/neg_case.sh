ed1362e5b001dfb31a993ed4ed08d38b3ae43a2f
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-18 11:40:58 +0300

    Scripts fix

--- lab_01_05_05/func_tests/scripts/neg_case.sh
+++ lab_01_05_05/func_tests/scripts/neg_case.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
 file_in=$1
-file_args=$3
+_args=$3
+memory_error=0
 
 if [[ (! -f "$file_in") ]]; then
-  exit 1
+  exit 100
 fi
 
-(../../main.o "$file_args" > "real_out.txt") < "$file_in"
+if [[ -n "$USE_VALGRIND" ]]; then
+    valgrind --log-file=../data/valgrind_pos.temp -q "../../app.exe" < "$file_in" > "real_out.txt" "$_args"
 
-exit $?
+    if [[ -s "valgrind_pos.temp" ]]; then
+        memory_error=1
+    fi
+else
+    (../../main.o "$_args" > "real_out.txt") < "$file_in"
+fi
+
+status_code=$?
+if  [[ $status_code == 0 ]]; then
+    if [[ $memory_error == 0 ]]; then
+        exit 2
+    else
+        exit 3
+    fi
+else
+    if [[ $memory_error == 1 ]]; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
