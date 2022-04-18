e04b2cb9a148fe7f2a173622e388db9a6aae0293
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-18 11:44:16 +0300

    Builds fix

--- lab_01_04_01/build_debug.sh
+++ lab_01_04_01/build_debug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gcc -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -lm main.c -o main.o
+gcc -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -c main.c
+
+gcc -o app.exe main.o -lm
