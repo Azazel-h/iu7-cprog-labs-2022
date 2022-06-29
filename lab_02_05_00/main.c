52d6665cd7062261d4c46098a95b9715e22545c8
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-06-29 18:17:49 +0300

    Task 5: Trying to find build error in struct

--- lab_02_05_00/main.c
+++ lab_02_05_00/main.c
@@ -6,74 +6,74 @@
 #define INPUT_ERROR -11
 #define SIZE_ERROR -12
 #define SIZE_INPUT_ERROR -13
-#define INVALID_ERROR -14
 
 typedef struct
 {
-    size_t len;
     int nums[N];
-} array;
+    int *start;
+    int *end;
+} array_t;
 
-int read_array(array *arr);
-int is_valid(array *arr);
-int find_product(array *arr);
+
+int read_array(array_t *arr);
+int calculate_result(array_t *arr);
 
 void get_errors(int status_code);
+
 
 int main()
 {
     int status_code = OK;
-    array arr;
-    if ((status_code = read_array(&arr)) || (status_code = is_valid(&arr)))
+    array_t arr;
+    arr.start = arr.nums;
+    arr.end = arr.nums;
+
+    if ((status_code = read_array(&arr)))
         get_errors(status_code);
     else
-        printf("%d\n", find_product(&arr));
+    {
+        printf("%d\n", calculate_result(&arr));
+    }
     return status_code;
 }
 
-int read_array(array *arr)
+int read_array(array_t *arr)
 {
     int status_code = OK;
-    if (scanf("%zu", &(arr->len)) != 1)
+    size_t len_;
+
+    if (scanf("%zu", &len_) != 1)
         status_code = SIZE_INPUT_ERROR;
-    else if (arr->len < 1 || arr->len > N)
+    else if (len_ < 1 || len_ > N)
         status_code = SIZE_ERROR;
     else
     {
-        for (size_t i = 0; i < arr->len; ++i)
+        for (size_t i = 0; i < len_; ++i)
         {
-            if (scanf("%d", arr->nums + i) != 1)
+            if (scanf("%d", arr->end) != 1)
             {
                 status_code = INPUT_ERROR;
-                i = arr->len;
+                break;
             }
+            arr->end++;
         }
     }
     return status_code;
 }
 
-int is_valid(array *arr)
+int calculate_result(array_t *arr)
 {
-    int status_code = INVALID_ERROR;
-    for (size_t i = 0; i < arr->len; ++i)
-        if (*(arr->nums + i) % 2)
-            status_code = OK;
-    return status_code;
-}
-
-int find_product(array *arr)
-{
-    int is_odd = 0, product = 1;
-    for (size_t i = 0; i < arr->len; ++i)
+    int *temp_start_ = arr->start, *temp_end_ = arr->end - 1, max = *arr->start;
+    int *mid_pointer_ = arr->start + (arr->end - arr->start + 1) / 2, sum;
+    while (temp_start_ < mid_pointer_)
     {
-        if (*(arr->nums + i) % 2)
-        {
-            product *= arr->nums[i];
-            if (!is_odd)
-                is_odd = 1;
-        }
+        sum = *temp_start_ + *temp_end_;
+        if (sum > max)
+            max = sum;
+        ++temp_start_;
+        --temp_end_;
     }
-    return product;
+    return max;
 }
 
 void get_errors(int status_code)
@@ -88,9 +88,6 @@ void get_errors(int status_code)
             break;
         case SIZE_INPUT_ERROR:
             printf("ERROR: Bad input size\n");
-            break;
-        case INVALID_ERROR:
-            printf("ERROR: No one valid element\n");
             break;
         default:
             printf("ERROR: Unknown error\n");
