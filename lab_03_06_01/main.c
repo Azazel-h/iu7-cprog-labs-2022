d14067cb673494f731ab27d6e52d00325d933779
Yasuhiro <yasuhiro.corp@gmail.com>
2022-08-25 08:43:57 +0300

    Task 6: Another algorithm

--- lab_03_06_01/main.c
+++ lab_03_06_01/main.c
@@ -28,6 +28,7 @@ typedef struct
 
 
 int read_matrix(matrix_t *matrix);
+int read_by_spiral(matrix_t *matrix);
 
 void print_array(array_t *arr);
 void print_matrix(matrix_t *matrix);
@@ -54,38 +55,48 @@ int read_matrix(matrix_t *matrix)
     if (scanf("%zu%zu", &(matrix->rows_count), &(matrix->columns_count)) != 2)
         status_code = SIZE_INPUT_ERROR;
     else if ((matrix->rows_count < 1 || matrix->rows_count > N) ||
-             (matrix->columns_count < 1 || matrix->columns_count > N) ||
-             (matrix->rows_count != matrix->columns_count))
+        (matrix->columns_count < 1 || matrix->columns_count > N) ||
+        (matrix->rows_count != matrix->columns_count))
         status_code = SIZE_ERROR;
     else
     {
         for (size_t m = 0; m < matrix->rows_count; ++m)
             (matrix->rows + m)->len = matrix->columns_count;
 
-        int matrix_n = matrix->columns_count;
-        for (int m = 0; m < matrix_n; m++)
+        status_code = read_by_spiral(matrix);
+    }
+    return status_code;
+}
+
+
+int read_by_spiral(matrix_t *matrix)
+{
+    int status_code = OK;
+    int s_row_index = 0, s_column_index = 0;
+    int e_row_index = matrix->rows_count, e_column_index = matrix->columns_count;
+    while (s_row_index < e_row_index && s_column_index < e_column_index)
+    {
+        for (int i = s_column_index; i < e_column_index; ++i)
+            if (scanf("%d", (matrix->rows + s_row_index)->nums + i) != 1)
+                return INPUT_ERROR;
+        s_row_index++;
+        for (int i = s_row_index; i < e_row_index; ++i)
+            if (scanf("%d", (matrix->rows+ i)->nums + (e_column_index - 1)) != 1)
+                return INPUT_ERROR;
+        e_column_index--;
+        if (s_row_index < e_row_index)
         {
-            for (int i = m; i < matrix_n; i++)
-            {
-                if (scanf("%d", (matrix->rows + m)->nums + i) != 1)
+            for (int i = e_column_index-1; i >= s_column_index; --i)
+                if (scanf("%d", (matrix->rows + (e_row_index-1))->nums + i) != 1)
                     return INPUT_ERROR;
-            }
-            for (int i = m + 1; i < matrix_n; i++)
-            {
-                if (scanf("%d", (matrix->rows + i)->nums + (matrix_n - 1)) != 1)
+            e_row_index--;
+        }
+        if (s_column_index < e_column_index)
+        {
+            for (int i = e_row_index-1; i >= s_row_index; --i)
+                if (scanf("%d", (matrix->rows + i)->nums + s_column_index) != 1)
                     return INPUT_ERROR;
-            }
-            for (int i = matrix_n - 2; i >= m; i--)
-            {
-                if (scanf("%d", (matrix->rows + matrix_n - 1)->nums + i) != 1)
-                    return INPUT_ERROR;
-            }
-            for (int i = matrix_n - 2; i >= m + 1; i--)
-            {
-                if (scanf("%d", (matrix->rows + i)->nums + m) != 1)
-                    return INPUT_ERROR;
-            }
-            matrix_n--;
+            s_column_index++;
         }
     }
     return status_code;
